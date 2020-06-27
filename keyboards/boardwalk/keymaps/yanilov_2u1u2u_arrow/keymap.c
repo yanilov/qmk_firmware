22da6f65ecdd5e87a0ffbeb21443e5de96fd4ee7
Yoav Yanilov <yoav.yanilov@similarweb.com>
2020-06-28 02:16:26 +0300

    additions to associative symbols

--- keyboards/boardwalk/keymaps/yanilov_2u1u2u_arrow/keymap.c
+++ keyboards/boardwalk/keymaps/yanilov_2u1u2u_arrow/keymap.c
@@ -18,10 +18,12 @@
 // Layer shorthand
 enum layer {
     _BASE,
-    _FN1,
-    _FN2
+    _POK3R,
+    _ASSOC
 };
 
+#define ASSOC MO(_ASSOC)
+#define POK3R MO(_POK3R)
 // Mac sleep
 #define __SLEEP S(LCTL(KC_POWER))
 
@@ -45,20 +47,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
         KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,  KC_PGUP,
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-        KC_CAPS, KC_LCTL, KC_LALT, KC_LGUI,      KC_SPC,      MO(1),        MO(2),       KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
+        KC_CAPS, KC_LCTL, KC_LALT, KC_LGUI,      KC_SPC,      ASSOC,        POK3R,       KC_RGUI, KC_RALT, KC_LEFT, KC_DOWN, KC_RGHT
     ),
-    [_FN1] = LAYOUT_2u1u2u_arrow(
-        KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,
-        _______, KC_DQUO, _______, KC_EQL,  KC_LPRN, KC_RPRN, _______, KC_PGUP, KC_UP,  KC_PGDN,  KC_PSCR, KC_SLCK, KC_PAUS, _______,
-        _______, KC_QUOT, KC_LBRC, KC_RBRC, _______, _______, KC_MINS, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_HOME, 
-        _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______, KC_MINS, _______, _______, _______, _______, _______, KC_END,
+    /* Pok3r-compatible layer, media buttons were shifted to be more ergonomic on ortho.
+    */
+     [_POK3R] = LAYOUT_2u1u2u_arrow(
+        KC_GRV,  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, KC_PGUP, KC_UP,   KC_PGDN, KC_PSCR, KC_SLCK, KC_PAUS, _______,
+        _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, KC_HOME, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, KC_END,  _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______,     _______,      _______,     _______,      _______, _______, _______, _______, _______
     ),
-    [_FN2] = LAYOUT_2u1u2u_arrow(
-        __SLEEP, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, KC_MPRV, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, KC_PSCR, KC_SLCK, KC_PAUS, _______,
-        _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______, _______, _______, _______, _______, _______, _______, _______, _______, 
+    /* Associative layer, plus explicit arrows
+    M,H - minus, hyphen. P - plus, Q - double quote. A - apostrophe(single-quote). U - underscore. G - grave accent(`).
+    R,T - round brackets
+    S,D - square brackets
+    C,V - curly brackets
+    missing - Tilde (~) is accessible by grave+shift, as T is taken by square brackets.
+    renamed - F (follow) for pipe operator
+    */
+    [_ASSOC] = LAYOUT_2u1u2u_arrow(
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, KC_DQUO, _______, KC_EQL,  KC_LPRN, KC_RPRN, _______, KC_UNDS, KC_UP,   _______, KC_PLUS, _______, _______, _______,
+        _______, KC_QUOT, KC_LBRC, KC_RBRC, KC_PIPE, KC_GRV,  KC_MINS, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, 
+        _______, _______, _______, KC_LCBR, KC_RCBR, _______, _______, KC_MINS, _______, _______, _______, _______, _______, _______,
         _______, _______, _______, _______,     _______,      _______,     _______,      _______, _______, _______, _______, _______
     )
 };
